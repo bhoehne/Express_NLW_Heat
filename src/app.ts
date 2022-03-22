484bc960d1c539a5e9c95b43037eaaac4dd01d39
JohnGalt <johngalt@pop-os.localdomain>
2022-03-22 00:07:59 -0300

    server ainda n funcionando

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import "dotenv/config"
 import express from "express"
-
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 import { routes } from "./routes"
 
-const app = express()
-app.use(express.json())
 
+const app = express()
+const serverHttp = http.createServer(app)
+app.use(cors())
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+app.use(express.json())
 app.use(routes)
+
+io.on("connection", socket => {
+  console.log('Usuario conectado no socket ' + socket.id)
+})
 
 app.get("/github", (request, response) => {
   response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
@@ -19,4 +32,5 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => console.log('Server is running'))
+
+export { serverHttp, io }
--- src/controllers/CreateMessageController.ts
+++ src/controllers/CreateMessageController.ts
@@ -1,9 +1,18 @@
-import { Request, Response } from "express"
+import { Request, response, Response } from "express"
 import { CreateMessageService } from "../service/CreateMessageService";
 
+import { io } from "../app";
 class CreateMessageController {
   async handle(request: Request, response: Response) {
     const { message } = request.body
+    const { user_id } = request
+    const service = new CreateMessageService()
+
+    const result = await service.execute(message, user_id)
+
+
+    return response.json(result)
+
   }
 }
 
